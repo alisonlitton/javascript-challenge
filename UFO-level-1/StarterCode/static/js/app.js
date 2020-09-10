447f424e0eccebc2fffb4f13e27cbd3cb200a00f
alisonlitton <63117075+alisonlitton@users.noreply.github.com>
2020-09-10 15:56:14 -0700

    updated code

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -2,88 +2,34 @@
 // Assign the data from `data.js` to a descriptive variable
 var tableData = data;
 
-//select table body
-var tbody = d3.select("ufo-table")
+//Select the body
+var tbody = d3.select("tbody")
+//Select the button 
+var submit = d3.select("#filter-btn")
 console.log("welcome to the UFO sightings page...the truth is HERE!")
 
-// loop through table using object entries
-function displayData(something){ 
-    tbody.text("")
-    something.forEach(function(et_sighting){
-    new_tr = tbody.append("tr")
-    Object.entries(et_sighting).forEach(function([key, value]){
-        new_td = new_tr.append("td").text(value)	
-    })
-})}
+// create event handlersloop through table using object entries
 
-displayData(tableData)
-
-console.log("test")
-
-
-
-
-
-
-// // Loop Through `data` and console.log each UFO sighting object
-// data.forEach(function(UFOSightings) {
-//   console.log(UFOSightings);
-// });
-
-// // // Use d3 to append one table row `tr` for each UFO Sighting object
-// data.forEach(function(UFOSightings) {
-//   console.log(UFOSightings);
-//   var row = tbody.append("tr");
-// });
-
-// // Use `Object.entries` to console.log each UFO sighting info
-// data.forEach(function(UFOSightings) {
-//   console.log(UFOSightings);
-//   var row = tbody.append("tr");
-
-//   Object.entries(UFOSightings).forEach(function([key, value]) {
-//     console.log(key, value);
-//   });
-// });
-
-// data.forEach((UFOSightings) => {
-//     var row = tbody.append("tr");
-//     Object.entries(UFOSightings).forEach(([key, value]) => {
-//       var cell = row.append("td");
-//       cell.text(value);
-//     });
-//   });
-
-
-
-// // // Select the form
-// var form = d3.select("#form");
-// var submit = d3.select("#filter-btn")
-
-// submit.on("click", function(){
-
-// })
-
-// // // Create event handlers 
-// filter-btn.on("click", runEnter);
-// form.on("submit",runEnter);
-
-// // Complete the event handler function for the form
-// function runEnter() {
-
-//   // Prevent the page from refreshing
-//   d3.event.preventDefault();
-  
-//   // Select the input element and get the raw HTML node
-//   var inputElement = d3.select("#datetime");
-
-//   // Get the value property of the input element
-//   var inputValue = inputElement.property("value");
-
-//   console.log(inputValue);
-//   console.log(UFOinfo);
-
-//   var filteredData = UFOinfo.filter(UFOinfo => UFOinfo.datetime === inputValue);
-
-//   console.log(filteredData);
-
+var filter = d3.select("#filter-btn");
+tableData.forEach((UFOSightings) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOSightings).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+filter.on("click", function(){
+    d3.event.preventDefault();
+    var select_date_time = d3.select("#datetime").property("value");  
+    console.log(select_date_time)  
+    
+tbody.html("")
+    var filteredData = tableData.filter(UFOSightings => UFOSightings.datetime === select_date_time);
+    filteredData.forEach((UFOSightings) => {
+        var row = tbody.append("tr");
+        Object.entries(UFOSightings).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+    });
+});
